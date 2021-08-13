8a786f959fc3c0e0124969170c7caecbd91c7417
Saffat Bokul <saffatbokul@gmail.com>
2021-08-13 18:31:10 +0600

    Update install-lamp-ubuntu.sh

--- install-lamp-ubuntu.sh
+++ install-lamp-ubuntu.sh
@@ -5,11 +5,10 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.16-1_all.deb
 sudo apt-get update
 
-sudo apt install mysql-client mysql-community-server mysql-server
-sudo mysql_secure_installation
+yes Y | sudo apt install mysql-server
 
 
-sudo apt install apache2 php php-mysql libapache2-mod-php php-cli
+yes Y | sudo apt install apache2 php php-mysql libapache2-mod-php php-cli
 
 # Allow to run Apache on boot up
 sudo systemctl enable apache2
@@ -27,4 +26,3 @@ sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-in
 
 sudo systemctl restart apache2
 
-mysql -u root -p
